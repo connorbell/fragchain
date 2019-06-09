dda277235d5853c5caa4894cf270352a28266c53
Connor Bell <connor.n.bell@gmail.com>
2019-06-09 12:11:24 -0400

    first commit might delete

---
+++ Assets/Chain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    [SerializeField]
+    List<ShaderPass> shaders = new List<ShaderPass>();
+
+    [SerializeField]
+    Vector2 baseResolution;
+    void Start()
+    {
+        foreach (ShaderPass shaderPass in shaders)
+        {
+            shaderPass.InitWithResolution(baseResolution);
+        }
+    }
+    void FixedUpdate()
+    {
+        shaders[0].Blit(null);
+
+        for (int i = 1; i < shaders.Count; i++)
+        {
+            shaders[i].Blit(shaders[i-1].renderTexture);
+        }
+    }
+}
---
+++ Assets/ShaderPass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderPass : MonoBehaviour
+{
+    [HideInInspector]
+    public Material Mat;
+    [SerializeField]
+    public float Scale;
+
+    [SerializeField]
+    bool backBufferEnabled = false;
+
+    public RenderTexture renderTexture;
+
+    public void InitWithResolution(Vector2 resolution)
+    {
+        renderTexture = new RenderTexture((int)(resolution.x * Scale), (int)(resolution.y * Scale), 24);
+    }
+
+    public void Blit(Texture source)
+    {
+        Graphics.Blit(source, renderTexture, Mat);
+    }
+}
